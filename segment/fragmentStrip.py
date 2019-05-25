c7a2b7971c1307e1e1ce5b7c440089dceae9c9fd
JohanOlsson99 <51018314+JohanOlsson99@users.noreply.github.com>
2019-05-25 16:14:57 +0200

    Add files via upload

--- segment/fragmentStrip.py
+++ segment/fragmentStrip.py
@@ -1,4 +1,8 @@
 from PIL import Image
+import math
+
+ImageArray = []
+heightArray = []
 
 def fragmentStrip(ImageJpg, savePath, widthArray, heightArray):
     white = 1;
@@ -40,6 +44,9 @@ def fragmentStrip(ImageJpg, savePath, widthArray, heightArray):
                     i = j
                     break
         i += 1
+    widthSize = widthEnd - widthStart
+    print(widthSize, "widthSize")
+    merge(widthSize, everyWord = True)
 
 
 def crop(startValueWidth, startValueHeight, endValueWidth, \
@@ -51,10 +58,41 @@ def crop(startValueWidth, startValueHeight, endValueWidth, \
         box = (startValueWidth, startValueHeight, endValueWidth, endValueHeight)
         try:
             a = im.crop(box)
-            a.save(savePath + ImageJpg[13:-4] + "_" +str(startValueHeight) \
-            + "_" + str(endValueHeight) + ".jpg")
+            #ImageArrayMetod(a)
+            ImageArray.append(a)
+            height_temp =  endValueHeight - startValueHeight
+            heightArray.append(height_temp)
+            #a.save(savePath + ImageJpg[13:-4] + "_" +str(startValueHeight) \
+            #+ "_" + str(endValueHeight) + ".jpg")
+            #a.show()
         except e:
             return
+
+        
+
+def merge(width, everyWord = False):
+    if(everyWord == True):
+        print("YAY")
+        i = 0
+        widthLow = width * 0.9
+        widthHigh = width * 1.1
+        print(widthLow, widthHigh, "low and high")
+        while True:
+            if(len(ImageArray) == i):
+                return
+            else:
+                
+                if(heightArray[i] > widthLow and heightArray[i] < widthHigh):
+                    scale(ImageArray[i])
+                else:
+                    if(i == len(ImageArray) - 1):
+                        scale(ImageArray[i])
+                        print(True)
+                    elif(heightArray[i] + heightArray[i+1] > widthLow and heightArray[i] + heightArray[i+1] < widthHigh):
+                        scale(ImageArray[i])
+                    else:
+                        print(False)
+                i += 1
 
 
 fragmentStrip("./Task1/train/004708932_00055_l0.jpg", "./Result", 0, [])
